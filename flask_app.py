748bbe0b198dfd95e88eb80e88a498337265bc38
Mukhammad Karimov <hey@karimoff.me>
2021-02-22 14:47:41 +1100

    Comment flask app

--- flask_app.py
+++ flask_app.py
@@ -1,21 +1,21 @@
-import flask
+# import flask
 
-from model import Batch, OrderLine, allocate
-from orm import start_mappers
+# from model import Batch, OrderLine, allocate
+# from orm import start_mappers
 
-from repository import SqlAlchemyRepository
+# from repository import SqlAlchemyRepository
 
 
-@flask.route.gubbins
-def allocate_endpoint():
-    session = start_mappers()
-    batches = SqlAlchemyRepository(session).list()
+# @flask.route.gubbins
+# def allocate_endpoint():
+#     session = start_mappers()
+#     batches = SqlAlchemyRepository(session).list()
 
-    lines = [
-        OrderLine(l["orderid"], l["sku"], l["qty"]) for l in request.params
-    ]
+#     lines = [
+#         OrderLine(l["orderid"], l["sku"], l["qty"]) for l in request.params
+#     ]
 
-    allocate(lines, batches)
+#     allocate(lines, batches)
 
-    session.commit()
-    return 201
+#     session.commit()
+#     return 201
